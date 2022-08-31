861e4d2e010b0e0d6eccda278c7fbdda84d0b1cb
Valentina-Maraio <valentina.maraio89@gmail.com>
2022-09-01 01:04:08 +0200

    one complete character

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,26 @@
 class MainController < ApplicationController
     def index
+        require 'net/http'
+        require 'json' 
+        @url = 'https://rickandmortyapi.com/api/character'
+        @uri = URI(@url)
+        @response = Net::HTTP.get(@uri)
+        @results = JSON.parse(@response)
+
+        #error
+        if @results.empty?
+            @empty_result = "Choose another character"
+        elsif !@results
+            @empty_result = "Choose another character"
+        else
+            @empty_result = @results['results'][0]['name']
+        end
+
+        #type
+        if @results['results'][0]['type'].empty?
+            @no_type = 'No type'
+        else
+            @no_type = @results['results'][0]['type']
+        end
     end
 end
